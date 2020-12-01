7e50bccf3484e2f36ae5c5316f5f84db12a6d8f5
serendatapy <arvaldivia@gmail.com>
2020-12-01 16:11:05 +0100

    feat:adding fields to users

--- server/models/models.users.js
+++ server/models/models.users.js
@@ -1,15 +1,21 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = (sequelize, DataTypes) => {
 
-const Users = sequelize.define('users', {
-  username: {
-    type: DataTypes.STRING
-  }
-})
+  const Users = sequelize.define('users', {
+    id: { primaryKey: true, type: DataTypes.UUID },
+    email: { type: DataTypes.TEXT, allowNull: false, unique: true, validate: { isEmail: true, notEmpty: true } },
+    password: { type: DataTypes.TEXT, allowNull: false, validate: { notEmpty: true } },
+    firstName: { type: DataTypes.TEXT, validate: { isAlpha: true } },
+    lastName: { type: DataTypes.TEXT, validate: { isAlpha: true } },
+  })
 
-Users.associate = (models) => {
+  Users.associate = (models) => {
     Users.hasMany(models.items, { onDelete: 'CASCADE' });
   };
 
-return Users;
+  Users.beforeCreate(user => user.id = uuid());
+
+  return Users;
 }
