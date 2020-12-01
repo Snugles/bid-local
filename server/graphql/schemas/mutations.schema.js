1f71ed80754d3e704010440e01be4a53e4d8eaa2
Madhushree Gupta <madhushree25@gmail.com>
2020-12-01 16:06:02 +0000

    items types schema added

--- server/graphql/schemas/mutations.schema.js
+++ server/graphql/schemas/mutations.schema.js
@@ -1,6 +1,6 @@
 module.exports = `
 type Mutation {
     createUser(email: String!,password: String!,firstName: String, lastName: String, phoneNumber:Int): User!
-    createItem(name: String!): Item!
+    createItem(name: String!, minPrice: Int!, description: String): Item!
     deleteItem(id: ID!): Boolean!
   }`;
