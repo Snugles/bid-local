01c59581d1aaa006f4f5be813ec43e4b87557a8c
Matt Haddon <matthaddon5@gmail.com>
2020-12-02 16:34:18 +0000

    gitignore 2

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,6 @@
 module.exports = {
   database: 'db_bid_local',
-<<<<<<< HEAD
   username: 'matthaddon',
   password: 'postgres123',
   dialect: 'postgres',
 };
-=======
-  username: 'user',
-  password: 'mysqlpass',
-  dialect: 'mysql',
-};
->>>>>>> c8586cdd04c41baa3f933db22e3894e305dba693
