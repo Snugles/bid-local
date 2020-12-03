7f8cec77b4fbba0e36ea716f4de89e868fda7cf4
Nikos Cocci <coccinikos@gmail.com>
2020-12-03 20:10:00 +0100

    added home queries and updated create item mutation

--- client/queries/addItem.js
+++ client/queries/addItem.js
@@ -2,18 +2,12 @@ import { gql } from '@apollo/client';
 
 export const CREATE_ITEM = gql`
 mutation create_item(
-  $name: String!
-  $minPrice: Int!
-	$description: String
-	$userId: String!
-  $categoryId: String
+  $userId: String!,
+  $item: ItemUpdate!
   ) {
   create_item(
-    name: $name,
-    minPrice: $minPrice,
-    description: $description,
     userId: $userId,
-    categoryId: $categoryId
+    item: $item
   ) {
     id,
     name,
