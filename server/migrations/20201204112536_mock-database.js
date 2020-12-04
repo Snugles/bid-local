8ad299901073d7a7b669e721c13a73fd153c51f2
serendatapy <arvaldivia@gmail.com>
2020-12-04 17:06:55 +0100

    working on migrations

---
+++ server/migrations/20201204112536_mock-database.js
@@ -0,0 +1,219 @@
+const Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable() => "categories", deps: []
+ * createTable() => "users", deps: []
+ * createTable() => "addresses", deps: [users]
+ * createTable() => "items", deps: [users, categories]
+ *
+ */
+
+const info = {
+  revision: 1,
+  name: 'mock-database',
+  created: '2020-12-04T11:25:36.878Z',
+  comment: '',
+};
+
+const migrationCommands = (transaction) => {
+  return [
+    {
+      fn: 'createTable',
+      params: [
+        'categories',
+        {
+          id: { type: Sequelize.UUID, field: 'id', primaryKey: true },
+          name: { type: Sequelize.STRING, field: 'name', unique: true },
+          createdAt: {
+            type: Sequelize.DATE,
+            field: 'createdAt',
+            allowNull: false,
+          },
+          updatedAt: {
+            type: Sequelize.DATE,
+            field: 'updatedAt',
+            allowNull: false,
+          },
+        },
+        { transaction },
+      ],
+    },
+    {
+      fn: 'createTable',
+      params: [
+        'users',
+        {
+          id: { type: Sequelize.UUID, field: 'id', primaryKey: true },
+          email: {
+            type: Sequelize.STRING,
+            field: 'email',
+            unique: true,
+            allowNull: false,
+          },
+          password: {
+            type: Sequelize.TEXT,
+            field: 'password',
+            allowNull: false,
+          },
+          firstName: { type: Sequelize.TEXT, field: 'firstName' },
+          lastName: { type: Sequelize.TEXT, field: 'lastName' },
+          phoneNumber: { type: Sequelize.STRING(25), field: 'phoneNumber' },
+          createdAt: {
+            type: Sequelize.DATE,
+            field: 'createdAt',
+            allowNull: false,
+          },
+          updatedAt: {
+            type: Sequelize.DATE,
+            field: 'updatedAt',
+            allowNull: false,
+          },
+        },
+        { transaction },
+      ],
+    },
+    {
+      fn: 'createTable',
+      params: [
+        'addresses',
+        {
+          id: { type: Sequelize.UUID, field: 'id', primaryKey: true },
+          firstLineAddress: {
+            type: Sequelize.TEXT,
+            field: 'firstLineAddress',
+            notEmpty: true,
+          },
+          secondLineAddress: {
+            type: Sequelize.TEXT,
+            field: 'secondLineAddress',
+          },
+          city: { type: Sequelize.STRING, field: 'city', notEmpty: true },
+          postcode: {
+            type: Sequelize.STRING,
+            field: 'postcode',
+            notEmpty: true,
+          },
+          country: { type: Sequelize.STRING, field: 'country', notEmpty: true },
+          userId: {
+            type: Sequelize.UUID,
+            onUpdate: 'CASCADE',
+            onDelete: 'NO ACTION',
+            references: { model: 'users', key: 'id' },
+            field: 'userId',
+            allowNull: false,
+          },
+          createdAt: {
+            type: Sequelize.DATE,
+            field: 'createdAt',
+            allowNull: false,
+          },
+          updatedAt: {
+            type: Sequelize.DATE,
+            field: 'updatedAt',
+            allowNull: false,
+          },
+        },
+        { transaction },
+      ],
+    },
+    {
+      fn: 'createTable',
+      params: [
+        'items',
+        {
+          id: { type: Sequelize.UUID, field: 'id', primaryKey: true },
+          name: { type: Sequelize.STRING, field: 'name', allowNull: false },
+          minPrice: { type: Sequelize.INTEGER, field: 'minPrice', default: 0 },
+          description: { type: Sequelize.TEXT, field: 'description' },
+          userId: {
+            type: Sequelize.UUID,
+            onUpdate: 'CASCADE',
+            onDelete: 'NO ACTION',
+            references: { model: 'users', key: 'id' },
+            field: 'userId',
+            allowNull: false,
+          },
+          categoryId: {
+            type: Sequelize.UUID,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+            references: { model: 'categories', key: 'id' },
+            allowNull: true,
+            field: 'categoryId',
+          },
+          createdAt: {
+            type: Sequelize.DATE,
+            field: 'createdAt',
+            allowNull: false,
+          },
+          updatedAt: {
+            type: Sequelize.DATE,
+            field: 'updatedAt',
+            allowNull: false,
+          },
+        },
+        { transaction },
+      ],
+    },
+  ];
+};
+
+const rollbackCommands = (transaction) => {
+  return [
+    {
+      fn: 'dropTable',
+      params: ['addresses', { transaction }],
+    },
+    {
+      fn: 'dropTable',
+      params: ['categories', { transaction }],
+    },
+    {
+      fn: 'dropTable',
+      params: ['items', { transaction }],
+    },
+    {
+      fn: 'dropTable',
+      params: ['users', { transaction }],
+    },
+  ];
+};
+
+const pos = 0;
+const useTransaction = true;
+
+const execute = (queryInterface, sequelize, _commands) => {
+  let index = pos;
+  const run = (transaction) => {
+    const commands = _commands(transaction);
+    return new Promise((resolve, reject) => {
+      const next = () => {
+        if (index < commands.length) {
+          const command = commands[index];
+          console.log(`[#${index}] execute: ${command.fn}`);
+          index++;
+          queryInterface[command.fn](...command.params).then(next, reject);
+        } else resolve();
+      };
+      next();
+    });
+  };
+  if (this.useTransaction) {
+    return queryInterface.sequelize.transaction(run);
+  }
+  return run(null);
+};
+
+module.exports = {
+  pos,
+  useTransaction,
+  up: (queryInterface, sequelize) => {
+    return execute(queryInterface, sequelize, migrationCommands);
+  },
+  down: (queryInterface, sequelize) => {
+    return execute(queryInterface, sequelize, rollbackCommands);
+  },
+  info,
+};
