125988ab4a316fc97f7f0e72133b7f5f9fa44012
serendatapy <arvaldivia@gmail.com>
2020-12-04 11:22:25 +0100

    feat(initialized sequelize cli): substituted some config files

--- server/temp/index.js
+++ server/temp/index.js
@@ -4,16 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const dbconfig = require('../config/dbConfig'); //configuration of database
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const sequelize = new Sequelize(dbconfig);
 
 fs
   .readdirSync(__dirname)
@@ -21,6 +16,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
+    // var model = sequelize['import'](path.join(__dirname, file));
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
