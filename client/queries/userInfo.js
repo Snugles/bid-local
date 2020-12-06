e4de41e793c3408f9721c07dcfd137111c6884dd
snugles <peter.r.ackroyd24@gmail.com>
2020-12-06 16:25:47 +0000

    added mutation for userInfo

---
+++ client/queries/userInfo.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const GET_EMAIL = gql`
+  query get_user_by_email ($email: String!) {
+    get_user_by_email(email: $email) {
+      id
+      email
+      phoneNumber
+      address {
+        firstLineAddress
+        secondLineAddress
+        city
+        postcode
+        country
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+mutation update_user (
+  $userId: String!
+  $user: UserUpdate!
+) {
+  update_user (
+    userId: $userId
+    user: $user
+  ){
+    id
+    phoneNumber
+    address{
+      firstLineAddress
+      secondLineAddress
+      city
+      postcode
+      country
+    }
+    email
+  }
+}
+`;
