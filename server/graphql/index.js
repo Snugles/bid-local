20017d88038c464e1b81528a5c4307696b2938ad
serendatapy <arvaldivia@gmail.com>
2020-12-07 21:32:53 +0100

    removed comments

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -11,7 +11,7 @@ const loggerPlugin = {
 
   // Fires whenever a GraphQL request is received from a client.
   requestDidStart (requestContext) {
-    console.log('Request started! Query:\n');
+    //console.log('Request started! Query:\n');
     //console.log(requestContext.request);
     return {
       didEncounterErrors (requestContext) {
@@ -24,22 +24,22 @@ const loggerPlugin = {
 
 
 const getMe = async req => {
-  console.log('Received Headers',req.headers);
+  //console.log('Received Headers',req.headers);
   const token = req.headers['x-token'];
-  console.log('Received Token:',token);
-  console.log('STARTING AUTH PROCESS:');
-  //if (token) {
-  try {
-    console.log('PROCESSING:');
-    const response = await jwt.verify(token, process.env.SECRET);
-    console.log('RESULT OF AUTHENTICATION:',response);
-    return response;
-  } catch (e) {
-    throw new AuthenticationError( //token will fail if invalid or expired
-      'Your session expired. Sign in again.',
-    );
+  //console.log('Received Token:',token);
+  //console.log('STARTING AUTH PROCESS:');
+  if (token) {
+    try {
+      console.log('PROCESSING:');
+      const response = await jwt.verify(token, process.env.SECRET);
+      console.log('RESULT OF AUTHENTICATION:',response);
+      return response;
+    } catch (e) {
+      throw new AuthenticationError( //token will fail if invalid or expired
+        'Your session expired. Sign in again.',
+      );
+    }
   }
-  //}
 };
 
 
