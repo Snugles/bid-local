4a80cc676adb75f30468e44507be35e3b1f73072
serendatapy <arvaldivia@gmail.com>
2020-12-07 11:46:10 +0100

    added sing-up functions

--- server/models/models.users.js
+++ server/models/models.users.js
@@ -4,12 +4,39 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = (sequelize, DataTypes) => {
 
   const Users = sequelize.define('users', {
-    id: { primaryKey: true, type: DataTypes.UUID },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true, notEmpty: true } },
-    password: { type: DataTypes.TEXT, allowNull: false, validate: { notEmpty: true } },
-    firstName: { type: DataTypes.TEXT },
-    lastName: { type: DataTypes.TEXT },
-    phoneNumber: { type: DataTypes.STRING(25), validate: { isNumeric: true } },
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 42],
+      }
+    },
+    firstName: {
+      type: DataTypes.TEXT
+    },
+    lastName: {
+      type: DataTypes.TEXT
+    },
+    phoneNumber: {
+      type: DataTypes.STRING(25),
+      validate: {
+        isNumeric: true
+      }
+    },
   });
 
   Users.associate = (models) => {
