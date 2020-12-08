69d79398beeb58df30a91e33d519cda66cee6134
serendatapy <arvaldivia@gmail.com>
2020-12-08 09:22:58 +0100

    Added isOwner Auth

--- server/graphql/resolvers/authorization.js
+++ server/graphql/resolvers/authorization.js
@@ -1,6 +1,15 @@
 const { ForbiddenError } = require('apollo-server');
 const { skip } = require('graphql-resolvers');
 
+export const isItemOwner = async (_,{ id },{ models, me }) => {
+  const item = await models.items.findByPk(id, { raw: true });
+
+  if (item.userId !== me.id) {
+    throw new ForbiddenError('Not authenticated as owner.');
+  }
+  return skip;
+};
+
 module.exports.isAuthenticated = (_, __, { me }) =>{
   console.log('Authenticating...');
   return me ? skip : new ForbiddenError('Not authenticated as user.');
